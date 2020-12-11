9c1c24e8abc202b6187725456ea7466126cffd51
minikuro <cctf@classcat.com>
2020-12-12 00:00:52 +0900

    scripts added.

---
+++ cc-preproc_data.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+export SPM_DIR="./spm"
+
+export DATA_DIR="./data"
+
+export PROCESSED_DATA="./data_prepro"
+
+mkdir -p $PROCESSED_DATA
+
+python create_pretraining_data.py --spm_model_file=${SPM_DIR}/spm_sample.model \
+  --input_file=${DATA_DIR}/*.txt \
+  --output_file=${PROCESSED_DATA}/train.tf_record \
+  --meta_data_file_path=${PROCESSED_DATA}/train_meta_data
+
+## tf_records and metadata files will stored in the `PROCESSED_DATA`
---
+++ cc-pretrain.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+export MODEL_DIR=base
+
+export PROCESSED_DATA=processed_data
+
+export MODEL_CONFIG=model_configs
+
+export PRETRAINED_MODEL=pretrained_model
+mkdir -p $PRETRAINED_MODEL
+
+
+python run_pretraining.py --do_train --strategy_type=one-cpu \
+  --albert_config_file=${MODEL_CONFIG}/base/config.json \
+  --num_train_epochs=1 \
+  --train_batch_size=1 \
+  --warmup_proportion=0.1 \
+  --input_files=${PROCESSED_DATA}/train.tf_record \
+  --meta_data_file_path=${PROCESSED_DATA}/train_meta_data \
+  --output_dir=${PRETRAINED_MODEL}
+#  --init_checkpoint=pretrained_model
+
+  #--output_dir=${PRETRAINED_MODEL} --train_batch_size=8 --num_train_epochs=3
+  #--output_dir=${PRETRAINED_MODEL} --strategy_type=mirror --train_batch_size=32 --num_train_epochs=3
