7a8a0444ca5b9f3b5735c72749d150df93d48dd4
minikuro <cctf@classcat.com>
2020-12-11 23:53:00 +0900

    spm

--- spm/spm_train.sh
+++ spm/spm_train.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-INPUT_FILE=$1
+#INPUT_FILE=$1
 
-spm_train --input=${INPUT_FILE} --model_prefix=spm_sample --vocab_size=100 --character_coverage=0.9995 --model_type=unigram
+spm_train --input=../data/sample.txt --model_prefix=spm_sample --vocab_size=100 --character_coverage=0.9995 --model_type=unigram
